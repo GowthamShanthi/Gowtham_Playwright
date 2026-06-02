e1cc088bd07035b0a7526302f352eb672ac6e983
Gowthamv <gowtham.shanthi@gmail.com>
2026-06-02 17:20:27 +0530

    Locators code

--- tests/locatorsinbuilt.spec.js
+++ tests/locatorsinbuilt.spec.js
@@ -12,28 +12,28 @@ await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/lo
 
 
 
-// // 1.  getbby alttext locator
+// 1.  getbby alttext locator
 
-//   const logo = await page.getByAltText('company-branding');
+  const logo = await page.getByAltText('company-branding');
 
-//  await expect(logo).toBeVisible();
+ await expect(logo).toBeVisible();
 
-//   //2.get by placeholder locator
+  //2.get by placeholder locator
 
-//   await page.getByPlaceholder('Username').fill('Admin');
-
-
-// //3.get by role locator
-
-// await page.getByRole('button',{type:'submit'}).click();
+  await page.getByPlaceholder('Username').fill('Admin');
 
 
-// //4.get by text locator
+//3.get by role locator
 
-// await expect(page.getByText('Login')).toBeVisible();
+await page.getByRole('button',{type:'submit'}).click();
 
 
-// //4.get by text locator
+//4.get by text locator
+
+await expect(page.getByText('Login')).toBeVisible();
+
+
+//4.get by text locator
 
 await page.getByText('Forgot your password? ').click();
 
