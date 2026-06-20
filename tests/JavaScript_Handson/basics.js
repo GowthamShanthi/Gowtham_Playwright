b10ab8f89ee87a46728f5631e336442615b2a72a
Gowthamv <gowtham.shanthi@gmail.com>
2026-06-20 16:17:52 +0530

    Java script bascis1

---
+++ tests/JavaScript_Handson/basics.js
@@ -0,0 +1,29 @@
+//Namachivaya
+
+console.log("Namachivaya Vaalga");
+
+
+let a =4;
+console.log(a)
+// This will eturn what is the type of variable a 
+console.log(typeof(a))
+
+
+let b = 234.6
+// This will eturn what is the type of variable n
+console.log(typeof(b))
+
+
+//String 
+
+let c = "Namachivaya Vaalga"
+console.log(typeof(c))
+
+//undefined
+let d
+console.log(typeof(d))
+
+//null
+
+let e = null;
+console.log(typeof(e));
